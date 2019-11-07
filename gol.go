14c678e716d40387588699703b328efbdfcd7e1a
Seunghun Lee <viper1259@gmail.com>
2019-11-07 15:57:06 +0000

    100 iteration doesn't work

--- gol.go
+++ gol.go
@@ -74,6 +74,7 @@ func distributor(p golParams, d distributorChans, alive chan []cell) {
 		}
 	}
 
+
 	// Make sure that the Io has finished any output before exiting.
 	d.io.command <- ioCheckIdle
 	<-d.io.idle
